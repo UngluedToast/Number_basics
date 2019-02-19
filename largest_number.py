a01cad79b9d7714dd4f17a20a2c32755afd30df8
Andrew Josey <JoganLosey@Gmail.com>
2019-02-19 15:42:58 -0500

    Added files of exercises

---
+++ largest_number.py
@@ -0,0 +1,27 @@
+big_list = [1,5,10,4,7]
+# numbers = 0
+# biggest
+# the_index = 0
+
+# while numbers < len(big_list):
+    
+#     the_index = the_index + 1
+#     numbers = numbers + 1
+
+# print(total)
+big = 0
+value = 0
+q = 0
+
+while big < len(big_list):
+    if value < big_list[0 + q]:
+        value = big_list[0 + q]
+    big = big + 1 
+    q = q + 1
+print(value)
+
+
+# go thru loop
+# compare number values, not index values themeselves
+#????
+# profit
---
+++ n_to_m.py
@@ -0,0 +1,9 @@
+start = input("please give me a number to start at  ")
+end = input("please give me a number to end at  ")
+
+
+
+
+while int(start) < int(end):
+    start = int(start) + 1
+    print(start)
