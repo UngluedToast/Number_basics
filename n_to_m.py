0e2849f64f2b21d976c3ec8647620355da116237
Andrew Josey <JoganLosey@Gmail.com>
2019-02-19 15:49:57 -0500

    Delete n_to_m.py

--- n_to_m.py
+++
@@ -1,9 +0,0 @@
-start = input("please give me a number to start at  ")
-end = input("please give me a number to end at  ")
-
-
-
-
-while int(start) < int(end):
-    start = int(start) + 1
-    print(start)
